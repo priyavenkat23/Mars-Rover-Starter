6a3c91c4587d9edeabbf7fb2f87883c025d6d417
priyavenkat23 <shanmugapriya.gunaseelan@gmail.com>
2024-02-21 19:55:03 -0600

    Mars rover updated version

--- rover.js
+++ rover.js
@@ -1,5 +1,46 @@
+const Message = require("./message");
+
 class Rover {
    // Write code here!
+
+   constructor(position){
+      this.position = position;
+      this.mode = 'NORMAL';
+      this.generatorWatts = 110;
+   }
+
+    receiveMessage(message){ 
+      
+     let modeResult=[];
+     let modeChng_LowPower = false;
+     for(let i=0;i<message.commands.length; i++){
+      let commandType = message.commands[i].commandType;
+      
+      if(commandType== 'STATUS_CHECK'){
+         modeResult.push({completed: true, roverStatus: {mode: this.mode, generatorWatts: 110, position: this.position}});
+         
+      }else if(commandType == 'MODE_CHANGE'){
+         if(message.commands[i].value == 'LOW_POWER'){
+            modeChng_LowPower= true;         
+         }
+         this.mode = message.commands[i].value;        
+         modeResult.push({completed: true});
+         
+      }else if(commandType=='MOVE'){
+         if(!modeChng_LowPower){
+            this.position = message.commands[i].value;
+            modeResult.push({completed: true});
+         }else{
+            modeResult.push({completed: false});
+         }
+         
+      }
+     }
+     
+      let reponse = {message: message.name, 
+                     results:modeResult};
+       return reponse;
+    }
 }
 
 module.exports = Rover;
